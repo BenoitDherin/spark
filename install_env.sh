0679b8323be90fe60b68488a481fcd22b7512d84
Benoit Dherin <benoit@keep.com>
2015-05-24 19:50:12 +0200

    install script updated as well as .gitignore

--- install_env.sh
+++ install_env.sh
@@ -5,9 +5,13 @@ SPARK_BINARIES="spark-1.3.1-bin-hadoop2.4"
 SPARK_MIRROR="http://d3kbcqa49mib13.cloudfront.net"
 SPARK_DIST="$SPARK_MIRROR/${SPARK_BINARIES}.tgz"
 
-test -d ${SPARK_DIST} || mkdir $SPARK_DIR
-cd $SPARK_DIR && wget -O - $SPARK_DIST | tar xzf -
+test -d $SPARK_DIR || 
+  {
+    wget -O - $SPARK_DIST | tar xzf - && 
+    mv $SPARK_BINARIES $SPARK_DIR
+  } || exit 0
 
+# Following: http://tecadmin.net/install-oracle-java-8-jdk-8-ubuntu-via-ppa/
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
